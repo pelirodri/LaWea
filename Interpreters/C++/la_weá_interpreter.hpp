0a3206e46759ec7035b50e2e7c2ef8376a5d6b88
Rodrigo Pelissier <pelirodri@icloud.com>
2018-10-01 00:01:55 -0300

    Remove comments

--- Interpreters/C++/la_weá_interpreter.hpp
+++ Interpreters/C++/la_weá_interpreter.hpp
@@ -20,14 +20,8 @@
 #include <vector>
 #include <string>
 
-/**
- * The class with all the La Weá interpreting behavior and properties.
- */
 class la_weá_interpreter {
 	public:
-		/**
- 		 * All the valid commands.
-         */
 		typedef enum {
 			maricón,
 			maraco,
@@ -47,82 +41,19 @@ class la_weá_interpreter {
 			mierda
 		} command_t;
 
-		/**
-		 * The names of all valid commands.
- 		 */
 		static const std::vector<std::u32string> command_names;
-
-		/**
-		 * All the characters a command may be composed of.
-		 */
 		static const std::u32string valid_chars;		
 
-		/**
-		 * Keeps track of the length of loop starting commands.
-		 */
 		int loop_starts_length;
-
-		/**
-		 * Keeps track of the length of loop ending commands.
-		 */
 		int loop_ends_length;
 
-		/**
-		 * The starting point for interpreting the code.
-		 * @param file_path the path to the file with the code
-		 * @note The file must have the .lw extension.
-		 */
 		void interpret(const char *);
 
-		/**
-		 * A utility function that retrieves the code.
-		 * @param file_path the path to the file with the code
-		 * @return The code, encoded in UTF-32
-		 */
 		std::u32string get_code(const char *);
-
-		/**
-		 * Checks the validity of the code and retrieves the commands.
-		 * @param code the code to parse
-		 * @return The commands found
-		 */
 		std::vector<command_t> parse_code(const std::u32string &);
-
-		/**
-		 * Gets command_t from name and performs some validation.
-		 * @param cmd_name the name of the command
-		 * @param cmd_idx the index of the command
-		 * @param row the row the command was found at
-		 * @param col the column the command was found at
-		 * @return The command if found
-		 */
 		command_t parse_command(const std::u32string &, int, long, long);
-
-		/**
-		 * Runs the code by interpreting the commands.
-		 * @param commands the commands to run
-		 */
 		void run_commands(const std::vector<command_t> &);
-
-		/**
-		 * A utility function to find the appropriate loop starting command.
-		 * @param commands the array of commands to search in
-		 * @param i the index to start searching at
-		 * @return The index of the loop starting command or -1 if not found
-		 */
 		int find_loop_start(const std::vector<command_t> &, int);
-
-		/**
-		 * A utility function to find the appropriate loop ending command.
-		 * @param commands the array of commands to search in
-		 * @param i the index to start searching at
-		 * @return The index of the loop ending command or -1 if not found
-		 */
 		int find_loop_end(const std::vector<command_t> &, int);
-
-		/**
-		 * A utility function that prints an error message and exits the program abnormally.
-		 * @param err_msg the error message to print
-		 */
 		void exit_interpreter(const std::string &);
 };
