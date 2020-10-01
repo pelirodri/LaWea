6ecc7e18bc38ae78f319f8a2742dff042f45144d
Rodrigo Pelissier <pelirodri@icloud.com>
2020-10-01 01:48:44 -0300

    Incorporate minor changes

--- Interpreters/Web/node_modules/fork-ts-checker-webpack-plugin-v5/lib/typescript-reporter/issue/TypeScriptIssueFactory.d 2.ts
+++
@@ -1,4 +0,0 @@
-import * as ts from 'typescript';
-import { Issue } from '../../issue';
-declare function createIssuesFromTsDiagnostics(diagnostics: ts.Diagnostic[]): Issue[];
-export { createIssuesFromTsDiagnostics };
--- Interpreters/Web/node_modules/fork-ts-checker-webpack-plugin-v5/lib/typescript-reporter/reporter/TypeScriptConfigurationParser.d 2.ts
+++
@@ -1,4 +0,0 @@
-import * as ts from 'typescript';
-import { TypeScriptConfigurationOverwrite } from '../TypeScriptConfigurationOverwrite';
-declare function parseTypeScriptConfiguration(configFileName: string, configFileContext: string, configOverwriteJSON: TypeScriptConfigurationOverwrite, parseConfigFileHost: ts.ParseConfigFileHost): ts.ParsedCommandLine;
-export { parseTypeScriptConfiguration };
