a6464f760f5a47fed583dde87879982c1de3461c
Rodrigo Pelissier <pelirodri@icloud.com>
2021-04-10 13:01:59 -0400

    Put commands in different file

--- Interpreters/C/include/la_wea_interpreter/la_weá_interpreter.h
+++ Interpreters/C/include/la_wea_interpreter/la_weá_interpreter.h
@@ -19,60 +19,11 @@
 
 #ifndef LA_WEÁ_INTERPRETER_H
 #define LA_WEÁ_INTERPRETER_H
+	#include "la_weá_command_t.h"
+
 	#include <stdio.h>
 	#include <stdint.h>
 	#include <stdbool.h>
-
-	/// All the valid commands.
-	typedef enum la_weá_command_t {
-		/// Decrements current cell value by 1.
-		maricón,
-
-		/// Decrements current cell value by 2.
-		maraco,
-
-		/// Increments current cell value by 1.
-		weón,
-
-		/// Increments current cell value by 2.
-		aweonao,
-
-		/// Sets current cell value to 0.
-		maraca,
-
-		/// Moves back one cell.
-		chucha,
-
-		/// Moves forward one cell.
-		puta,
-
-		/// If current cell value is 0, moves the instruction pointer after the matching tula command.
-		pichula,
-
-		/// If current cell value is not 0, moves the instruction pointer after the matching pichula command.
-		tula,
-
-		/// Moves the instruction pointer after the closest tula command, regardless of the current cell value.
-		pico,
-
-		/// Prints current cell value to STDOUT as an ASCII character.
-		ctm,
-
-		/// Reads ASCII character from STDIN and stores it in the current cell.
-		quéweá,
-
-		/// Prints current cell value to STDOUT as an integer.
-		chúpala,
-
-		/// Reads integer from STDIN and stores it in the current cell.
-		brígido,
-
-		/// Copies current cell value if there is no copy; otherwise, pastes the copied value and resets the copy.
-		perkin,
-
-		/// Terminates program.
-		mierda
-	} la_weá_command_t;
 
 	/**
 	 * The entry point for interpreting the code.
--- Interpreters/C/src/lib/la_wea_interpreter/interpret_commands.h
+++ Interpreters/C/src/lib/la_wea_interpreter/interpret_commands.h
@@ -19,7 +19,7 @@
 
 #ifndef INTERPRET_COMMANDS_H
 #define INTERPRET_COMMANDS_H
-	#include "la_weá_interpreter.h"
+	#include "la_weá_command_t.h"
 
 	void interpret_commands(la_weá_command_t *);
 #endif
