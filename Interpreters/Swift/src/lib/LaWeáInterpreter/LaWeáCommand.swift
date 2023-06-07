b9feca1d3a57df479924ad131b5093378f7294b8
pelirodri <pelirodri@icloud.com>
2023-06-07 00:34:28 -0400

    Remove whitespaces

--- Interpreters/Swift/src/lib/LaWeáInterpreter/LaWeáCommand.swift
+++ Interpreters/Swift/src/lib/LaWeáInterpreter/LaWeáCommand.swift
@@ -21,49 +21,34 @@
 public enum LaWeáCommand: String, CaseIterable {
     /// Decrements current cell value by 1.
     case maricón
-    
     /// Decrements current cell value by 2.
     case maraco
-    
     /// Increments current cell value by 1.
     case weón
-    
     /// Increments current cell value by 2.
     case aweonao
-    
     /// Sets current cell value to 0.
     case maraca
-    
     /// Moves back one cell.
     case chucha
-    
     /// Moves forward one cell.
     case puta
-    
     /// If current cell value is 0, moves the instruction pointer after the matching tula command.
     case pichula
-    
     /// If current cell value is not 0, moves the instruction pointer after the matching pichula command.
     case tula
-    
     /// Moves the instruction pointer after the closest tula command, regardless of the current cell value.
     case pico
-    
     /// Prints current cell value to STDOUT as an ASCII character.
     case ctm
-    
     /// Reads ASCII character from STDIN and stores it in the current cell.
     case quéweá
-    
     /// Prints current cell value to STDOUT as an integer.
     case chúpala
-    
     /// Reads integer from STDIN and stores it in the current cell.
     case brígido
-    
     /// Copies current cell value if there is no copy; otherwise, pastes the copied value and resets the copy.
     case perkin
-    
     /// Terminates program.
     case mierda
 }
